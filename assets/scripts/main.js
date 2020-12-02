933c983522d641862042d225b82a499a21b917f5
Savanna Vu <vuksavanna@berkeley.edu>
2020-12-02 02:34:34 -0800

    updated content & style; fixed +/-

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -22,6 +22,7 @@ function toggleProduceTips(produceId) {
    
    parent.classList.toggle('cover');
    child.classList.toggle('hidden');
+   
 }
 
 /** closes all expanded produce tips when a different one is clicked */ 
@@ -30,9 +31,14 @@ function hideProduceTips(produceId) {
 
    for (var i = 0; i < produceElements.length; i++) {
       var currElement = produceElements[i];
-      if ((currElement.classList.contains('cover') === false) && (currElement.id !== produceId)) { //if a produce item (besides the one the user is currently openeing) is open 
+      if ((currElement.classList.contains('cover') === false) && (currElement.id !== produceId)) { //if a produce item (besides the one the user is currently openeing) is open, close it
          currElement.classList.add('cover');
          currElement.children[1].classList.add('hidden');
+         //new
+         if (currElement.children[0].className === "q-n-a") {
+            currElement.children[0].children[1].classList.toggle('off');
+            currElement.children[0].children[2].classList.toggle('off');
+         }
       }
    }
 }
@@ -43,25 +49,23 @@ function hideProduceTips(produceId) {
 
 /** toggles +/- */
 function toggleAccordion(produceId) {
-   switchOtherIcons(produceId);
+   // switchOtherIcons(produceId);
    let plus = document.getElementById(produceId).children[0].children[1];
    let minus = document.getElementById(produceId).children[0].children[2];
    
-   plus.classList.toggle('on');
+   
    plus.classList.toggle('off');
-   minus.classList.toggle('on');
    minus.classList.toggle('off');
 }
 
 /** switches all other - --> + when diff a is revealed */ 
-function switchOtherIcons(produceId) {
-   var produceElements = document.querySelectorAll('.produce');
+// function switchOtherIcons(produceId) {
+//    var produceElements = document.querySelectorAll('.produce');
 
-   for (var i = 0; i < produceElements.length; i++) {
-      var currElement = produceElements[i];
-      if ((currElement.children[0].children[2].classList.contains('off') === false) && (currElement.children[0].parentElement.id !== produceId)) { 
-         toggleAccordion(produceId);
-         console.log("hi");
-      }
-   }
-}
+//    for (var i = 0; i < produceElements.length; i++) {
+//       var currElement = produceElements[i];
+//       if ((currElement.children[0].children[2].classList.contains('off') === false) && (currElement.children[0].parentElement.id !== produceId)) { 
+//          toggleAccordion(produceId);
+//       }
+//    }
+// }
