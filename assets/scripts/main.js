a55878b3ed7542736adfd98d04dd4b8de1d68698
Savanna Vu <vuksavanna@berkeley.edu>
2020-12-02 01:49:13 -0800

    q&n complete

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,4 @@
 //
-//display page after loaded
-//
-
-/*
-https://stackoverflow.com/questions/9550760/hide-page-until-everything-is-loaded-advanced
-*/
-
-//
 //Setting current pg/active button to orange
 //
 
@@ -21,6 +13,17 @@ function activeButton (buttonID) {
 //For fruit/veggie button, have height expand & rm hidden
 //
 
+/** opens/closes produce tips */ 
+function toggleProduceTips(produceId) {
+   hideProduceTips(produceId);
+
+   let parent = document.getElementById(produceId);
+   let child = parent.children[1];
+   
+   parent.classList.toggle('cover');
+   child.classList.toggle('hidden');
+}
+
 /** closes all expanded produce tips when a different one is clicked */ 
 function hideProduceTips(produceId) {
    var produceElements = document.querySelectorAll('.produce');
@@ -34,14 +37,31 @@ function hideProduceTips(produceId) {
    }
 }
 
-/** opens/closes produce tips */ 
-function toggleProduceTips(produceId) {
-   hideProduceTips(produceId);
+//
+//For q&a, toggle accordion icon
+//
 
-   let parent = document.getElementById(produceId);
-   let child = parent.children[1];
+/** toggles +/- */
+function toggleAccordion(produceId) {
+   switchOtherIcons(produceId);
+   let plus = document.getElementById(produceId).children[0].children[1];
+   let minus = document.getElementById(produceId).children[0].children[2];
    
-   parent.classList.toggle('cover');
-   child.classList.toggle('hidden');
+   plus.classList.toggle('on');
+   plus.classList.toggle('off');
+   minus.classList.toggle('on');
+   minus.classList.toggle('off');
 }
 
+/** switches all other - --> + when diff a is revealed */ 
+function switchOtherIcons(produceId) {
+   var produceElements = document.querySelectorAll('.produce');
+
+   for (var i = 0; i < produceElements.length; i++) {
+      var currElement = produceElements[i];
+      if ((currElement.children[0].children[2].classList.contains('off') === false) && (currElement.children[0].parentElement.id !== produceId)) { 
+         toggleAccordion(produceId);
+         console.log("hi");
+      }
+   }
+}
